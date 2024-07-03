004ed83194bfecc5673bddf8e33277703078f7b8
ROPERO17 <146777519+ROPERO17@users.noreply.github.com>
2024-07-03 12:25:40 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["the", "our", "my", "her"];
+  let adjetivos = ["great", "big", "cute", "quiet"];
+  let sustantivos = ["jogger", "racoon", "cat", "dog"];
+
+  let oraciones = [];
+
+  for (let pronombre of pronombres) {
+    for (let adjetivo of adjetivos) {
+      for (let sustantivo of sustantivos) {
+        let oracion = `${pronombre}${adjetivo}${sustantivo}.com`;
+        oraciones.push(oracion);
+      }
+    }
+  }
+  oraciones.forEach(oracion => console.log(oracion));
 };
